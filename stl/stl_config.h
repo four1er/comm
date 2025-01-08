8f7ab5a99b98dde8caf7ab8ba5da92ba02d6e6d6
forytian <forytian@tencent.com>
2025-01-08 08:03:53 +0000

    add

--- stl/stl_config.h
+++ stl/stl_config.h
@@ -8,3 +8,7 @@
 #define __THROW_BAD_ALLOC             \
   fprintf(stderr, "out of memory\n"); \
   exit(1)
+
+#define __STL_REQUIRES(__type_var, __concept) \
+  do {                                        \
+  } while (0)
