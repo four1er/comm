bfd1611de3959dd2e94d55c2a3db76398afaa1d3
four1er <920242373@qq.com>
2025-01-10 22:07:57 +0800

    add deque

---
+++ stl_v1/stl_deque.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <algorithm>
+#include <initializer_list>
+#include <memory>
+
+#include "iterator.h"
+#include "stl_alloc.h"
+#include "stl_config.h"
+#include "type_traits"
