9eab6931198b381c86136907dae65f89b19a8ef3
four1er <920242373@qq.com>
2025-01-11 23:16:08 +0800

    add deque

--- stl_v1/stl_deque.h
+++ stl_v1/stl_deque.h
@@ -52,4 +52,71 @@ struct deque_iterator {
 
   deque_iterator(const iterator& x)
       : cur(x.cur), first(x.first), last(x.last), node(x.node) {}
+
+  reference operator*() const { return *cur; }
+  pointer operator->() const { return &(operator*()); }
+
+  difference_type operator-(const self& iter) const {
+    return difference_type(buffer_size()) * (node - iter.node - 1) +
+           (cur - first) + (iter.last - iter.cur);
+  }
+
+  self& operator++() {
+    ++cur;
+    if (cur == last) {
+      set_node(node + 1);
+      cur = first;
+    }
+  };
+
+  self operator++(int) {
+    self tmp = *this;
+    ++*this;
+    return tmp;
+  }
+
+  self& operator--() {
+    if (cur == first) {
+      set_node(node - 1);
+      cur = last;
+    }
+    --cur;
+    return *this;
+  }
+  self operator--(int) {
+    self tmp = *this;
+    --*this;
+    return tmp;
+  }
+
+  self& operator+=(difference_type n) {
+    difference_type offset = n + (cur - first);
+    if (offset > 0 && offset < buffer_size()) {
+      cur += n;
+    } else {
+      difference_type node_offset =
+          offset > 0 ? offset / difference_type(buffer_size())
+                     : -difference_type((-offset - 1) / buffer_size()) - 1;
+      set_node(node + node_offset);
+      cur = first + (offset - node_offset * difference_type(buffer_size()));
+    }
+    return *this;
+  }
+
+  self operator+(difference_type n) const {
+    self tmp = *this;
+    return tmp += n;
+  }
+  self& operator-=(difference_type n) { return *this += -n; }
+  self operator-(difference_type n) {
+    self tmp = *this;
+    return tmp -= n;
+  }
+  reference operator[](difference_type n) const { return *(*this + n); }
+
+  bool operator==(const self& iter) const { return cur == iter.cur; }
+  bool operator!=(const self& iter) const { return !(*this == iter); }
+  bool operator<(const self& iter) const {
+    return (node == iter.node) ? (cur < iter.cur) : (node < iter.node);
+  }
 };
